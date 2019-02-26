576bb42521da25dc3c58c9116394b143ea790cbb
toni <tonivalle15@gmail.com>
2019-02-26 21:16:45 +0100

    Home Page

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$ (document).ready(function(){
+
+  $('#abbutton').on('click', function(){
+    window.location="./about-me.html";
+  });
+  $('#resbutton').on('click', function(){
+    window.location="./resume.html";
+  });
+});
