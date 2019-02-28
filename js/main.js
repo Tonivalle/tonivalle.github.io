6dcf4b295e43ed865a02be74f39e8cda89500412
toni <tonivalle15@gmail.com>
2019-02-28 13:28:17 +0100

    Resume changes. Logo modifications.

--- js/main.js
+++ js/main.js
@@ -6,4 +6,21 @@ $ (document).ready(function(){
   $('#resbutton').on('click', function(){
     window.location="./resume.html";
   });
+
+  $(window).scroll(function() {
+    var x = $(window).scrollTop();
+
+    if (x <= 100) {
+      $(".navbar-brand img").css("display", "none");
+      // $(".navbar-brand").show();
+      $(".logo").css("display", "block");
+      // setTimeout(function(){$(".logo").css("z-index", "10000")},300);
+    } else {
+      $(".navbar-brand img").css("display", "block");
+      // $(".navbar-brand").hide();
+      $(".logo").css("display", "none");
+      // $(".logo").css("z-index", "0");
+    }
+  });
+
 });
